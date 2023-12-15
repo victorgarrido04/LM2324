28dc0798f67bbbf32dbacd16b6210b9f931005aa
victorgarrido04 <victorgarrido04@iesamachado.org>
2023-12-15 14:24:46 +0100

    ...

---
+++ tema3/fichero.js
@@ -0,0 +1,4 @@
+// Conjunto de funciones relaziadas por Víctor Garrido Jiménez para el siti web http://127.0.0.1:5500/tema3/javascript3.html
+function saludo(){
+    alert("Hola mi gente der tisto!")
+}
