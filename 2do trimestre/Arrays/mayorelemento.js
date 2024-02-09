503801e4995dff34824244761c720ec4da45d4bd
victorgarrido04 <victorgarrido04@iesamachado.org>
2024-02-09 13:52:57 +0100

    ...

--- 2do trimestre/Arrays/mayorelemento.js
+++ 2do trimestre/Arrays/mayorelemento.js
@@ -15,3 +15,9 @@ function menor(vector){
     }
 return menor;
 }
+
+function imprimir(vector){
+    for(var contador=0; contador<vector.length;contador++){
+        document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + vector[contador] + "<br>"
+    }
+}
